178b62095279ecb3cb5c11cc63a29c05e7a96be8
David Jensen <david@dkjensen.com>
2021-06-15 11:20:08 -0400

    feat: first commit

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
 if ( ! defined( 'STARTER_THEME_VERSION' ) ) {
-	$starter_theme_version = wp_get_theme()->get( 'Version' );
+	$starter_theme_version = ! empty( wp_get_theme()->get( 'Version' ) ) ? wp_get_theme()->get( 'Version' ) : '0.0.0-development';
 
 	// Replace the version number of the theme on each release.
 	define( 'STARTER_THEME_VERSION', $starter_theme_version );
